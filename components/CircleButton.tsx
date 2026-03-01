10f02410fafad6896012d04723314c6fde9b93f6
Sourave halder <souravh093@gmail.com>
2026-03-01 14:16:51 +0600

    Add emoji functionality: implement EmojiPicker, EmojiList, and EmojiSticker components; enhance image selection with stickers and options

---
+++ components/CircleButton.tsx
@@ -0,0 +1,37 @@
+import { Pressable, StyleSheet, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+type Props = {
+  onPress: () => void;
+};
+
+const CircleButton = ({ onPress }: Props) => {
+  return (
+    <View style={styles.circleButtonContainer}>
+      <Pressable style={styles.circleButton} onPress={onPress}>
+        <MaterialIcons name="add" size={38} color={"#25292e"} />
+      </Pressable>
+    </View>
+  );
+};
+
+export default CircleButton;
+
+const styles = StyleSheet.create({
+  circleButtonContainer: {
+    width: 84,
+    height: 84,
+    marginHorizontal: 60,
+    borderWidth: 4,
+    borderColor: "#2f6ecc",
+    borderRadius: 42,
+    padding: 3,
+  },
+  circleButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 42,
+    backgroundColor: "#fff",
+  },
+});
---
+++ components/IconButton.tsx
@@ -0,0 +1,30 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+type Props = {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  label: string;
+  onPress: () => void;
+};
+
+const IconButton = ({ icon, label, onPress }: Props) => {
+  return (
+    <Pressable style={styles.iconButton} onPress={onPress}>
+      <MaterialIcons name={icon} size={24} color={"#fff"} />
+      <Text style={styles.iconButtonLabel}>{label}</Text>
+    </Pressable>
+  );
+};
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  iconButton: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconButtonLabel: {
+    color: "#fff",
+    marginTop: 12,
+  },
+});
